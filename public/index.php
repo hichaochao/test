bfec3bf4f0d87020f814cd0c6a1c6f4b75ff7e33
DESKTOP-UBFIQD6\86159 <chao605702409@qq.com>
2024-04-28 19:46:43 +0800

    测试

--- public/index.php
+++ public/index.php
@@ -15,7 +15,7 @@
 | our application. We just need to utilize it! We'll simply require it
 | into the script here so that we don't have to worry about manual
 | loading any of our classes later on. It feels nice to relax.
-|
+|//
 */
 
 require __DIR__.'/../bootstrap/autoload.php';
